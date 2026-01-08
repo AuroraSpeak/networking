687b4593d6a0fb64cc0b06ea37bb239e831927cd
22peacemaker <karpinskijannis@gmail.com>
2026-01-08 14:33:48 +0100

    Implemented udp client connect functionality

--- pkg/client/cilent.go
+++ pkg/client/cilent.go
@@ -1,4 +1,30 @@
 package client
 
+import (
+	"fmt"
+	"net"
+)
+
 type Client struct {
+	Host string
+	Port int
+
+	conn *net.UDPConn
+}
+
+func NewClient(Host string, Port int) *Client {
+	return &Client{}
+}
+
+func (c *Client) Connect() error {
+	conncetionString := fmt.Sprintf("%s:%d", c.Host, c.Port)
+	s, err := net.ResolveUDPAddr("udp4", conncetionString)
+	if err != nil {
+		return err
+	}
+	c.conn, err = net.DialUDP("udp", nil, s)
+	if err != nil {
+		return err
+	}
+	return nil
 }
