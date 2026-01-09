367fdb642b5dafbd535c36a38f28614c6d3b3221
22peacemaker <karpinskijannis@gmail.com>
2026-01-09 21:58:51 +0100

    chore: Vite-Konfiguration aktualisieren

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -12,6 +12,19 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      '/ws': {
+        target: 'ws://localhost:8080/ws',
+        changeOrigin: true,
+        ws: true,
+      },
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
