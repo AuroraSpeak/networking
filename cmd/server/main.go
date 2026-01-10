efb81b56f4b079560cf3ba236124772039328132
22peacemaker <karpinskijannis@gmail.com>
2026-01-10 21:49:42 +0100

    chore(config): update air config and main entry points

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"context"
 
+	"github.com/aura-speak/networking/pkg/protocol"
 	"github.com/aura-speak/networking/pkg/server"
 )
 
 func main() {
 	ctx := context.Background()
 	server := server.NewServer(8080, ctx)
-	server.OnPacket("", func(packet []byte) error {
+	server.OnPacket(protocol.PacketTypeDebugAny, func(packet *protocol.Packet, clientAddr string) error {
 		server.Broadcast(packet)
 		return nil
 	})
